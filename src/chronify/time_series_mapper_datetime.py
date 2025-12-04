d07c5a64000a50adb542131b9714368ca2399226
Daniel Thom <daniel.thom@nrel.gov>
2025-12-03 18:16:26 -0700

    Add mypy exception

--- src/chronify/time_series_mapper_datetime.py
+++ src/chronify/time_series_mapper_datetime.py
@@ -106,12 +106,17 @@ class MapperDatetimeToDatetime(TimeSeriesMapperBase):
                 # get standard time zone of to_tz
                 to_tz_std = get_standard_time_zone(to_tz)
                 # tz-naive time does not have skips/dups, so always localize in std tz first
-                ser_from = ser_from.dt.tz_localize(to_tz_std).dt.tz_convert(to_tz)
+                # mypy complains with error: "Properties" has no attribute "tz_localize"
+                # but the type is pandas.core.indexes.accessors.DatetimeProperties and the
+                # attribute exists.
+                ser_from = ser_from.dt.tz_localize(to_tz_std).dt.tz_convert(to_tz)  # type: ignore
+                pass
             case (False, True):
                 # get standard time zone of fm_tz
                 fm_tz_std = get_standard_time_zone(fm_tz)
                 # convert to standard time zone of fm_tz before making it tz-naive
-                ser_from = ser_from.dt.tz_convert(fm_tz_std).dt.tz_localize(to_tz)
+                ser_from = ser_from.dt.tz_convert(fm_tz_std).dt.tz_localize(to_tz)  # type: ignore
+                pass
         match (self._adjust_interval, self._wrap_time_allowed):
             case (True, _):
                 ser = pd.Series(
